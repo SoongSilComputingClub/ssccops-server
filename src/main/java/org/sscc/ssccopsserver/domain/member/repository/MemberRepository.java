11f668382058b19be0d0d8b42d01dca135abebf5
swthewhite <swthewhite@gmail.com>
2026-08-13 05:01:15 +0900

    #2 feat(member): MemberEntity에 Supabase 식별자 컬럼 추가

--- src/main/java/org/sscc/ssccopsserver/domain/member/repository/MemberRepository.java
+++ src/main/java/org/sscc/ssccopsserver/domain/member/repository/MemberRepository.java
@@ -1,6 +1,12 @@
 package org.sscc.ssccopsserver.domain.member.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.sscc.ssccopsserver.domain.member.entity.MemberEntity;
 
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {}
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findBySpbUserId(UUID spbUserId);
+}
